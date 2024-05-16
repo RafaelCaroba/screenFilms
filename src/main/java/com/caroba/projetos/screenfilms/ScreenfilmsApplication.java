0dff53cb969008bf2d9a7b655bc38d7915efc99a
unknown <rafa.caroba.17@gmail.com>
2024-05-16 15:00:49 -0300

    primeiro commit

---
+++ src/main/java/com/caroba/projetos/screenfilms/ScreenfilmsApplication.java
@@ -0,0 +1,13 @@
+package com.caroba.projetos.screenfilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenfilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenfilmsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/caroba/projetos/screenfilms/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.caroba.projetos.screenfilms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/screenfilms");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        adapter.setShowSql(true);
+        adapter.setPrepareConnection(true);
+        adapter.setGenerateDdl(true);
+        return adapter;
+
+    }
+}
